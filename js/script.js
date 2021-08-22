24b12aae7bb1c78f9590d6c4e82494f9f0ea76cc
sakawatchy434 <hossainshakawat434@gmail.com>
2021-08-21 21:50:53 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
+/* Memory-part  */
 function totalMemoryCost(product) {
     const costOfMemory = document.getElementById('memory-cost');
     costOfMemory.innerText = product;
     const costOfMemoryAmount = parseInt(costOfMemory.innerText);
     return costOfMemoryAmount;
-    // getTotalValueOfInput()
 };
+/* storage-part  */
 function totalStorageCost(product) {
     const costOfMemory = document.getElementById('storage-cost');
     costOfMemory.innerText = product;
     const costOfMemoryAmount = parseInt(costOfMemory.innerText);
     return costOfMemoryAmount;
-    // getTotalValueOfInput()
 };
+/* delivery-part  */
 function totalDeliveryCost(product) {
     const costOfMemory = document.getElementById('delivery-cost');
     costOfMemory.innerText = product;
     const costOfMemoryAmount = parseInt(costOfMemory.innerText);
     return costOfMemoryAmount;
-    // getTotalValueOfInput()
 };
-
 function getTotalInput() {
+    /*  calculation-total-cost-of-sold-start  */
     const bestPrice = document.getElementById('best-cost').innerText;
     const memoryCost = document.getElementById('memory-cost').innerText;
     const storageCost = document.getElementById('storage-cost').innerText;
     const deliveryCost = document.getElementById('delivery-cost').innerText;
     const totalCost = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost);
-    const totalCostOfSold = document.getElementById('total-cost').innerText = (totalCost);
+    const totalCostOfSold = document.getElementById('total-cost').innerText = totalCost;
+    /* footer-total-part-start  */
+    document.getElementById('footer-total').innerText = totalCostOfSold;
+    /* footer-total-part-end. */
     return totalCostOfSold;
-
+    /*  calculation-total-cost-of-sold-end.  */
 }
+/* Promo-code-part-start */
+const promoApply = document.getElementById('apply-btn').addEventListener('click', function () {
+    const promoInputFiled = document.getElementById('input-value');
+    const promoCodeValue = promoInputFiled.value;
+    const discountTotalFiled = document.getElementById('footer-total');
+    const grandTotal = discountTotalFiled.innerText;
+    if (promoCodeValue.toLowerCase() == 'stevekaku') {
+        discountTotalFiled.innerText = (grandTotal - (grandTotal * 0.2));
+        promoInputFiled.value = '';
+    }
+});
+/* Promo-code-part-end */
 
+/* mamorey-part-start  */
 document.getElementById('memory-first').addEventListener('click', function () {
     const firstMemoryCost = totalMemoryCost(0);
     getTotalInput()
@@ -39,6 +55,8 @@ document.getElementById('memory-second').addEventListener('click', function () {
     const secondMemoryCost = totalMemoryCost(180);
     getTotalInput()
 });
+/*mamorey-part-end  */
+/*storage-part-start  */
 document.getElementById('storage-first').addEventListener('click', function () {
     firstStorageCost = totalStorageCost(0);
     getTotalInput()
@@ -51,6 +69,8 @@ document.getElementById('storage-thired').addEventListener('click', function () 
     secondStorageCost = totalStorageCost(180);
     getTotalInput()
 });
+/*storage-part-end  */
+/*delivery-part-start  */
 document.getElementById('delivery-first').addEventListener('click', function () {
     firstDeliveryCost = totalDeliveryCost(0);
     getTotalInput()
@@ -59,3 +79,5 @@ document.getElementById('delivery-second').addEventListener('click', function ()
     secondDeliveryCost = totalDeliveryCost(20);
     getTotalInput()
 });
+/*delivery-part-end  */
+
